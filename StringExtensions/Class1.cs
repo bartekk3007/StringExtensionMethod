7a6d51d1fcdf4cac86116a4a07f0beb0a1cf5077
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-06-18 23:48:41 +0200

    Add files via upload

---
+++ StringExtensions/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace StringExtensions
+{
+    public static class Extensions
+    {
+        public static string AppendExclamation(this string str)
+        {
+            return str + "!";
+        }
+    }
+}
---
+++ StringExtensionsUsage/Program.cs
@@ -0,0 +1,14 @@
+﻿using StringExtensions;
+
+namespace StringExtensionsUsage
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Bartek";
+            string appendedName = name.AppendExclamation();
+            Console.WriteLine(appendedName);
+        }
+    }
+}
